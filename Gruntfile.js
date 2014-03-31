273c8fc5e8df3d14e231a525fc89617d4599dbb8
Fabrice Bentz <fbentz@MacBook-Air-de-Fabrice.local>
2014-03-31 18:04:29 +0200

    add browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,26 @@ module.exports = function (grunt) {
   'use strict';
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  var DEST_DIR = 'build/';
+  var SRC_DIR = 'src/';
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    dest: DEST_DIR,
+    src: SRC_DIR,
+
     jshint: {
       options: {
         jshintrc: true
       },
       files: ['Gruntfile.js']
+    },
+
+    browserify: {
+      dist: {
+
+      }
     }
   });
 
