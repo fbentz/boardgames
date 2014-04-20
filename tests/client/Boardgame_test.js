9f90fcec63bf1f4a0288c0faecce113028ddbbf0
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-20 20:46:54 +0200

    Update test label

--- tests/client/Boardgame_test.js
+++ tests/client/Boardgame_test.js
@@ -4,7 +4,7 @@ var Boardgame = require('../../src/client/lib/boardgame/model/BoardgameModel');
 
 describe('Boardgame Model', function () {
 
-  it('should create a boardgame class', function() {
+  it('should instanciate a boardgame model', function() {
     var boardgame = new Boardgame();
     expect(boardgame).to.be.an.instanceof(Boardgame);
     expect(boardgame.get('type')).to.equal('boardgame');
