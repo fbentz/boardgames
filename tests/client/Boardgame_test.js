85f158e9adcf48628d343c185a1ce378293dcdfe
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-20 18:25:37 +0200

    Add test to project with mocha
        - Add test to BoardgameModel

---
+++ tests/client/Boardgame_test.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+var Boardgame = require('../../src/client/lib/boardgame/model/BoardgameModel');
+
+describe('Boardgame Model', function () {
+
+  it('should create a boardgame class', function() {
+    var boardgame = new Boardgame();
+    expect(boardgame).to.be.an.instanceof(Boardgame);
+    expect(boardgame.get('type')).to.equal('boardgame');
+  });
+});
