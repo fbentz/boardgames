14cb5505dd6d55d7644c748a10e4c9484d2c93f3
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-20 18:51:19 +0200

    Add type to BoardgameModel

--- src/client/lib/boardgame/model/BoardgameModel.js
+++ src/client/lib/boardgame/model/BoardgameModel.js
@@ -1,5 +1,8 @@
 var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
-  url: 'http://localhost:5984/'
+  url: 'http://localhost:5984/',
+  defaults: {
+    'type': 'boardgame'
+  }
 });
