b351d8c1516cf9bc8da638be98f693df65dda8bf
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-21 10:13:40 +0200

    Add BoardgameListView

---
+++ src/client/lib/boardgame/views/BoardgameListView.js
@@ -0,0 +1,15 @@
+var Parent = require('base').View;
+
+module.exports = Parent.extend({
+  template: require('../templates/boardgameList.hbs'),
+  initialize: initialize,
+  render: render
+});
+
+function initialize() {
+
+}
+
+function render() {
+
+}
