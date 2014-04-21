5a42df94dcb131be7d23ca5c42dce5eb37cc1e4a
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-21 10:03:50 +0200

    change url to boardgame model

--- src/client/lib/boardgame/model/BoardgameModel.js
+++ src/client/lib/boardgame/model/BoardgameModel.js
@@ -1,7 +1,7 @@
 var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
-  url: 'http://localhost:5984/',
+  url: 'http://localhost:5984/boardgamesapp/',
   defaults: {
     'type': 'boardgame'
   }
