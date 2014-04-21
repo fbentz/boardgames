c85079f130d8ab1db842e6fec5c0e76f98716a3b
Fabrice BENTZ <fabrice.bentz@free.fr>
2014-04-21 23:50:07 +0200

    Add temporary dev

---
+++ src/client/lib/boardgame/views/BoardgameItemView.js
@@ -0,0 +1,16 @@
+var Parent = require('base').View;
+
+module.exports = Parent.extend({
+  template: require('../templates/boardgameTableItem.hbs'),
+  initialize: initialize,
+  render: render
+});
+
+function initialize() {
+  this.render();
+}
+
+function render() {
+  this.$el.html(this.template(this.model.toJSON()));
+  return this;
+}
